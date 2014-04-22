4545300cda32504c0ca99a9d6de20fce226b1ce9
Gil Rutter <g.o.rutter@warwick.ac.uk>
2014-04-22 14:52:53 +0100

    Accept command line arguments and remind user how I work.

--- REMD/generate_temps.py
+++ REMD/generate_temps.py
@@ -1,18 +1,45 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
 '''Little script to return a list of temperatures of replicas, based on interpolating a function you give'''
 from numpy import linspace, round, zeros, interp
-import numpy
+from argparse import ArgumentParser
+from sys import argv
+
+parser = ArgumentParser(description='Generate a list of temperatures which'\
+                                   +' obey the density function passed in.')
+parser.add_argument('N', nargs='?', default=None, type=int,
+      help='How many temperatures to generate.')
+parser.add_argument('--temps', nargs='+', default=[275.0,350.0],
+      help='Temperatures at which the densities given apply.')
+parser.add_argument('--densities', nargs='+', default=[1.0, 1.0],
+      help='Densities at the specified temperatures.')
+
+if len(argv) == 1:
+   parser.parse_args(['-h'])
+
+args = parser.parse_args()
+
+print args
+print "N =", args.N
+print "Temps =", args.temps
+print "Densities =", args.densities
+print ""
+
+try:
+   assert (len(args.temps) == len(args.densities))
+except AssertionError:
+   print "ERROR: --temps arg length doesn't match --densities."
+   exit()
 
 #settings
-n_replicas=30
 samples=100000
-x_points = [275, 288, 305, 322, 350]
-density_points = [0.5,1.0,3.0,1.0,0.5]
 
 linear_temps = linspace(275,350,samples)
-density = interp( linear_temps, x_points, density_points )
-density_norm = (density*(n_replicas-1))/density.sum()
+density = interp( linear_temps, args.temps, args.densities )
+density_norm = (density*(args.N-1))/density.sum()
 
-nonlinear_temps=zeros(n_replicas)
+nonlinear_temps=zeros(args.N)
 count=1.0
 j=0
 
@@ -27,5 +54,7 @@ for i in range(samples):
 
 nonlinear_temps[-1]=linear_temps[-1]
 
+print "Temperature list:"
 for temp in round(nonlinear_temps, 2):
-   print temp
+   print temp,
+print ""
