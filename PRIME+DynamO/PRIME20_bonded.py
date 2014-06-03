063a9e9a5efe7238a49edacde7387305e3913eac
Gil Rutter <g.o.rutter@warwick.ac.uk>
2014-06-03 20:12:16 +0100

    First working commit
    Seems to output valid working configurations now.
    - Pseudobond parameters in PRIME20_bonded.py have been changed to
      maintain angles CO-CH-SC and NH-CH-SC rather than maintain their ratio
      with CH-SC. This works better.
    - mylammps.py function create_coords now takes an argument for the space
      between each NH called res_space, and an argument for
      prototype positions called prototype_positions, and uses these
      sensibly.
    - peptide_maker.py has been significantly changed to arrive at a working
      state.

--- PRIME+DynamO/PRIME20_bonded.py
+++ PRIME+DynamO/PRIME20_bonded.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 #Units of angstrom
 BB_BB_diam = {'NH CH':'1.46', 'CH CO':'1.51', 'CO NH':'1.33'}
 BB_BB_diam.update({'CH NH':BB_BB_diam['NH CH'], 'CO CH':BB_BB_diam['CH CO'], 'NH CO':BB_BB_diam['CO NH']})
@@ -5,18 +7,13 @@ BB_BB_diam.update({'CH NH':BB_BB_diam['NH CH'], 'CO CH':BB_BB_diam['CH CO'], 'NH
 BB_pseudo = {'NH CO':'2.45', 'CH NH':'2.41', 'CO CH': '2.45', 'CH CH': '3.80'}
 BB_pseudo.update( {'CO NH':'2.45', 'NH CH':'2.41', 'CH CO': '2.45'} )
 BB_SC_diam = {}
-
-#Incorrect values for the time being...
-for SC in list('ACDEFHIKLMNPQRSTVWY'):
-    BB_SC_diam['CH ' + SC] = '1.531'
-    BB_SC_diam[SC + ' CH'] = '1.531'
-    BB_pseudo['NH ' + SC] = '2.44'
-    BB_pseudo[SC + ' NH'] = '2.44'
-    BB_pseudo['CO ' + SC] = '2.49'
-    BB_pseudo[SC + ' CO'] = '2.49'
+BB_SC_well_diam = {}
+BB_SC_pseudo_diam = {}
+BB_SC_pseudo_well_diam = {}
 
 #My own parameter set, see PRIME20_parametrisation dir
-BB_SC_diam = {
+BB_SC_ideal_pseudobondlens = {}
+BB_SC_ideal_bondlens = {
     'CH A':'1.57', 'A CH':'1.57',
     'CH R':'4.23', 'R CH':'4.23',
     'CH N':'2.53', 'N CH':'2.53',
@@ -40,26 +37,53 @@ BB_SC_diam = {
 
 #numbers from http://0-onlinelibrary.wiley.com.pugwash.lib.warwick.ac.uk/doi/10.1002/prot.1100/full
 #(Alpha helix formation)
-#rationale: keep the ratio intact
+
+##rationale: keep the ratio intact
+#for SC in list('ACDEFHIKLMNPQRSTVWY'):
+    #BB_SC_ideal_pseudobondlens['NH ' + SC] = str(2.44*(float(BB_SC_ideal_bondlens['CH ' + SC])/1.531))
+    #BB_SC_ideal_pseudobondlens[SC + ' NH'] = str(2.44*(float(BB_SC_ideal_bondlens['CH ' + SC])/1.531))
+
+    #BB_SC_ideal_pseudobondlens['CO ' + SC] = str(2.49*(float(BB_SC_ideal_bondlens['CH ' + SC])/1.531))
+    #BB_SC_ideal_pseudobondlens[SC + ' CO'] = str(2.49*(float(BB_SC_ideal_bondlens['CH ' + SC])/1.531))
+
+#Rationale, keep angles CO-CH-SC and NH-CH-SC intact
 for SC in list('ACDEFHIKLMNPQRSTVWY'):
-    BB_pseudo['NH ' + SC] = str(2.44*(float(BB_SC_diam['CH ' + SC])/1.531))
-    BB_pseudo['CO ' + SC] = str(2.49*(float(BB_SC_diam['CH ' + SC])/1.531))
-    BB_pseudo[SC + ' NH'] = str(2.44*(float(BB_SC_diam['CH ' + SC])/1.531))
-    BB_pseudo[SC + ' CO'] = str(2.49*(float(BB_SC_diam['CH ' + SC])/1.531))
+
+    CH_SC_dist = float(BB_SC_ideal_bondlens['CH ' + SC])
+
+    BB_SC_ideal_pseudobondlens['NH ' + SC] = sqrt(2.1316 + CH_SC_dist**2 + (CH_SC_dist*1.05) )
+    BB_SC_ideal_pseudobondlens[SC + 'NH '] = BB_SC_ideal_pseudobondlens['NH ' + SC]
+
+    BB_SC_ideal_pseudobondlens['CO ' + SC] = sqrt(2.2801 + CH_SC_dist**2 + (CH_SC_dist*1.12) )
+    BB_SC_ideal_pseudobondlens[SC + 'CO '] = BB_SC_ideal_pseudobondlens['CO ' + SC]
+
+
+
+#################################
+#  create diams and well diams  #
+#################################
+for key in BB_SC_ideal_bondlens:
+    BB_SC_diam[key] = str(float(BB_SC_ideal_bondlens[key])*0.97625)
+    BB_SC_well_diam[key] = str(float(BB_SC_ideal_bondlens[key])*1.02375)
+
+for key in BB_SC_ideal_pseudobondlens:
+    BB_SC_pseudo_diam[key] = str(float(BB_SC_ideal_pseudobondlens[key])*0.97625)
+    BB_SC_pseudo_well_diam[key] = str(float(BB_SC_ideal_pseudobondlens[key])*1.02375)
 
 #For printing out LAMMPS parameters:
-#def plus1(i):
-    #while True:
-        #i+=1
-        #yield i
+if __name__ == "__main__":
+    def plus1(i):
+        while True:
+            i+=1
+            yield i
 
-#go=plus1(7)
+    go=plus1(7)
 
-#for SC in list('ACDEFHIKLMNPQRSTVWY'):
-    #for BB in ['NH','CH','CO']:
-        #try:
-            #len=BB_pseudo[BB+' '+SC]
-        #except:
-            #len=BB_SC_diam[BB+' '+SC]
-        #print "bond_coeff   {3:2}   90.0         {0:.4}   #{1}-{2}".format(len,SC,BB, go.next())
+    for SC in list('ACDEFHIKLMNPQRSTVWY'):
+        for BB in ['NH','CH','CO']:
+            try:
+                len=BB_SC_ideal_bondlens[BB+' '+SC]
+            except:
+                len=BB_SC_ideal_pseudobondlens[BB+' '+SC]
+            print "bond_coeff   {3:2}   90.0         {0:.4}   #{1}-{2}".format(len,SC,BB, go.next())
 
