2e349cfdd08e9eeb52334ebfadb16b262606dc91
Gil Rutter <g.o.rutter@warwick.ac.uk>
2014-08-18 14:33:02 +0100

    Clearer comments.

--- dcd_to_pdb.sh
+++ dcd_to_pdb.sh
@@ -5,19 +5,29 @@ if [ $# -lt 3 ]; then
     exit
 fi
 
-DCD_file=$1
-DCD_filename=$(basename "$DCD_file")
-DCD_filename_noext="${DCD_file%.*}"
+################################
+#  Settings from command-line  #
+################################
 
+DCD_file=$1
 PSF_file=$2
 stride=$3
 
-#Other settings
+######################
+#  Derived settings  #
+######################
+
+DCD_filename=$(basename "$DCD_file")
+DCD_filename_noext="${DCD_file%.*}"
+
 output_prefix=${DCD_filename_noext}"-"${stride}
 PDB_traj=${output_prefix}".pdb"
 temp=${output_prefix}".temp.pdb"
 
-#Check DCD exists
+###########################
+#  Check DCD file exists  #
+###########################
+
 if [ -f $DCD_file ]; then
     true
 else
@@ -25,12 +35,17 @@ else
     exit
 fi
 
-##############################
-#  Make PDB files from DCDs  #
-##############################
+#############################
+#  Use catdcd to make PDBs  #
+#############################
 
 echo "Converting DCD file to PDB file."
 catdcd -o ${PDB_traj} -otype pdb -stride ${stride} -stype psf -s ${PSF_file} ${DCD_file}
+
+############################
+#  Fix up PDBs with regex  #
+############################
+
 echo "Using regex to fix the PDB trajectory file."
 cat ${PDB_traj} | perl -pe 's{^[^A].*$}{$n=$n+1; "ENDMDL\nMODEL     $n"}e' | tail +2 | head --lines=-1 >$temp
 cat $temp | perl -pe 's/(ATOM +\d+)    [41]/\1  N  /' | perl -pe 's/(ATOM +\d+)    2/\1  CA /' | perl -pe 's/(ATOM +\d+)    3/\1  C  /' | perl -pe 's/(ATOM +\d+) +\d+ +(\w)/\1  \2SC \2/' >${PDB_traj}
