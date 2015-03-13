79b22b3669f2658259590d58b400c04c412799e1
Gil Rutter <g.o.rutter@warwick.ac.uk>
2015-03-13 19:32:27 +0000

    PSF file now mandatory. Possible to specify and ID for replex sims.

--- PRIME+DynamO/snapshots_to_pdb.sh
+++ PRIME+DynamO/snapshots_to_pdb.sh
@@ -4,33 +4,43 @@
 #  Settings  #
 ##############
 
-xyz=temp.xyz
-pdb=out.pdb
-temp=temp.temp
 max_args=1000
 
 ##################
 #  Get psf file  #
 ##################
 
-if [ -z "$1" ]; then
-    psf=(*.psf)
-else
-    psf=$1
+if [ $# -lt 1 ]; then
+    echo "Usage: $0 [psf file] {ID number}"
+    exit
 fi
 
+psf=$1
+ID=$2
+
 echo "PSF = $psf."
-echo "If wrong pass it as the first argument."
 
 ###################################
 #  Collect and order input files  #
 ###################################
 
-all_files=(Snapshot.*.xml.bz2)
-snapshot_files=${all_files[@]//*output*}
+if [[ $ID == '' ]]; then
+    all_files=(Snapshot.*.xml.bz2)
+    startpos=2
+    xyz=temp.xyz
+    pdb=out.pdb
+else
+    all_files=(Snapshot.ID${ID}.*.xml.bz2)
+    startpos=3
+    xyz=temp.ID${ID}.xyz
+    pdb=out.ID${ID}.pdb
+fi
 
+snapshot_files=${all_files[@]//*output*}
 ordered_snapshot_files=(`for file in ${snapshot_files[@]}; do echo "$file"; done\
-    | sort -n --field-separator=. -k2`)
+    | sort -n --field-separator=. -k${startpos}`)
+
+echo ${ordered_snapshot_files[*]}
 
 n_snapshot_files=${#ordered_snapshot_files[*]}
 
@@ -61,9 +71,9 @@ catdcd -o ${pdb} -otype pdb -stype psf -s $psf -xyz $xyz
 echo ""
 
 echo "Using regex to fix the PDB trajectory file."
-cat ${pdb} | perl -pe 's{^[^A].*$}{$n=$n+1; "ENDMDL\nMODEL     $n"}e' | sed 1d | sed '$d' >$temp
-cat $temp | perl -pe 's/(ATOM +\d+)    [41]/\1  N  /' | perl -pe 's/(ATOM +\d+)    2/\1  CA /' | perl -pe 's/(ATOM +\d+)    3/\1  C  /' | perl -pe 's/(ATOM +\d+) +\d+ +(\w)/\1  \2SC \2/' >${pdb}
-rm -f $temp $xyz
+cat ${pdb} | perl -pe 's{^[^A].*$}{$n=$n+1; "ENDMDL\nMODEL     $n"}e' | sed 1d | sed '$d' >${pdb}.temp
+cat ${pdb}.temp | perl -pe 's/(ATOM +\d+)    [41]/\1  N  /' | perl -pe 's/(ATOM +\d+)    2/\1  CA /' | perl -pe 's/(ATOM +\d+)    3/\1  C  /' | perl -pe 's/(ATOM +\d+) +\d+ +(\w)/\1  \2SC \2/' >${pdb}
+rm -f ${pdb}.temp $xyz
 echo ""
 
 echo "Done. Check: $pdb"
