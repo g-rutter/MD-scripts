740da7dc3ff2f3f171eaee9746e21d70c5e90e7c
Gil Rutter <g.o.rutter@warwick.ac.uk>
2015-07-24 15:06:00 +0100

    Implement use of pickles

--- pdb_to_Ramachandran.py
+++ pdb_to_Ramachandran.py
@@ -7,6 +7,7 @@
 import Bio.PDB as bp
 import math
 import matplotlib
+import pickle
 #Not interactive, can use over SSH
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
@@ -19,12 +20,18 @@ import argparse
 ##############
 
 parser = argparse.ArgumentParser(description='Make Ramachandran out of pdb trajectory')
-parser.add_argument('-b', '--bins', type=int, nargs='?', default=200,
+parser.add_argument('-b', '--bins', type=int, nargs=1, default=200,
                     help='Number of bins for Ramachandran in each dimension.')
 parser.add_argument('--nocolour', '-n', action='store_true', default=False)
 parser.add_argument('--skip', '-s', type=int, default=1)
 parser.add_argument('--notnormed', action='store_true', default=False)
-parser.add_argument('PDB_file',  type=str, nargs='+')
+parser.add_argument('PDB_file',  type=str, nargs='*')
+
+parser.add_argument('-p', '--pickle', type=str, nargs=1,
+                    help='Pickle the heatmap so it won\'t have to be calculated again. Provide the pickle filename.')
+
+parser.add_argument('-u', '--unpickle', type=str, nargs=1,
+                    help='Unpickle the heatmap from the provided filename so it doesn\'t have to be calculated now.')
 
 args = parser.parse_args()
 print args
@@ -35,7 +42,20 @@ nocolour=args.nocolour
 notnormed=args.notnormed
 pdb_files=args.PDB_file
 
-out_file = 'Ramachandran_' + pdb_files[0] + '.png'
+#validation
+if len(pdb_files) == 0 and args.unpickle == None:
+    print "Either provide PDB file(s) or a Python pickle for the heatmap."
+    print "Try -h"
+    exit()
+
+if args.unpickle != None and args.pickle != None:
+    print "Can't both pickle and unpickle. Pick one!"
+    exit()
+
+if len(pdb_files) > 0:
+    out_file = 'Ramachandran_' + pdb_files[0] + '.png'
+else:
+    out_file = 'Ramachandran_' + args.unpickle[0] + '.png'
 
 ########################
 #  Rads->Degrees func  #
@@ -66,6 +86,7 @@ psis_all = []
 i=0
 
 for pdb_file in pdb_files:
+    print pdb_file
     for model in bp.PDBParser().get_structure("", pdb_file):
 
         if i%skip == 0:
@@ -78,8 +99,16 @@ for pdb_file in pdb_files:
                         psis_all.append( degrees(-phi_psi[1]))
         i+=1
 
+if args.unpickle == None:
+    heatmap, xedges, yedges = np.histogram2d(psis_all, phis_all, bins=bins, normed=False, range=[[-180,180]]*2)
+else:
+    with open(args.unpickle[0]) as unpickle_file:
+        heatmap = pickle.load(unpickle_file)
 
-heatmap, xedges, yedges = np.histogram2d(psis_all, phis_all, bins=bins, normed=False, range=[[-180,180]]*2)
+if args.pickle != None:
+    with open(args.pickle[0], 'w') as pickle_file:
+        pickle.dump(heatmap, pickle_file)
+
 #print heatmap
 
 if notnormed == False:
@@ -91,8 +120,11 @@ if notnormed == False:
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-ax.set_ylabel(r'$\psi$', fontsize=20)
-ax.set_xlabel(r'$\phi$', fontsize=20)
+ax.set_ylabel(r'$\psi$', fontsize=30)
+ax.set_xlabel(r'$\phi$', fontsize=30)
+
+ax.tick_params(axis='x', labelsize=20)
+ax.tick_params(axis='y', labelsize=20)
 
 ax.xaxis.set_ticks( [-180, -120, -60, 0, 60, 120, 180] )
 ax.yaxis.set_ticks( [-180, -120, -60, 0, 60, 120, 180] )
