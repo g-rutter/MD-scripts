8a5bfc56028aae9a8935117f1e48079a22c8f1f8
Gil Rutter <g.o.rutter@warwick.ac.uk>
2015-08-19 15:13:18 +0100

    pdb Rama diff is now pickle-only.

--- pdb_Rama_diff.py
+++ pdb_Rama_diff.py
@@ -4,6 +4,7 @@
 #Note minus sign in Psis - could be because PDBParser is calcing it oddly
 #types of interpolation: bilinear, nearest, bicubic
 
+import pickle
 import Bio.PDB as bp
 import math
 import matplotlib
@@ -19,23 +20,19 @@ import argparse
 ##############
 
 parser = argparse.ArgumentParser(description='Make Ramachandran out of pdb trajectory')
-parser.add_argument('-b', '--bins', type=int, nargs='?', default=200,
-                    help='Number of bins for Ramachandran in each dimension.')
-parser.add_argument('--skip', '-s', type=int, default=1)
-parser.add_argument('PDB_file',  type=str, nargs=2)
+parser.add_argument('unpickle_file', type=str, nargs=2,
+                    help='Unpickle the heatmap from the provided filename so it doesn\'t have to be calculated now.')
+parser.add_argument('--textsize', '-t', type=int, nargs='?', default=23,
+                    help='Using 23 as standard in journals.')
 
 args = parser.parse_args()
 print args
 
-bins=args.bins
-skip=args.skip
-pdb_files=args.PDB_file
+unpickle_files=args.unpickle_file
 
-out_file = 'Rama_diff_' + pdb_files[0] + 'vs' + pdb_files[1] + '.png'
+out_file = 'Rama_diff_' + unpickle_files[0] + 'vs' + unpickle_files[1] + '.png'
 
-print "Processing difference Ramachandran plot of", pdb_files[0], "minus", pdb_files[1]
-print "Bins:", bins
-print "Skip:", skip
+print "Processing difference Ramachandran plot of", unpickle_files[0], "minus", unpickle_files[1]
 
 ########################
 #  Rads->Degrees func  #
@@ -60,35 +57,17 @@ def degrees(rad_angle) :
 #  Extract phi,psi from pdbs, make 2D hist  #
 #############################################
 
-phis_all = [ [], [] ]
-psis_all = [ [], [] ]
-
-i=0
-
-for i_pdb, pdb_file in enumerate(pdb_files):
-    for model in bp.PDBParser().get_structure("", pdb_file):
-
-        if i%skip == 0:
-            for chain in model:
-                poly = bp.Polypeptide.Polypeptide(chain)
-
-                for phi_psi in poly.get_phi_psi_list():
-                    if phi_psi[0] != None and phi_psi[1] != None:
-                        phis_all[i_pdb].append( degrees(phi_psi[0]))
-                        psis_all[i_pdb].append( degrees(-phi_psi[1]))
-        i+=1
-
-
-heatmap1, xedges, yedges = np.histogram2d(psis_all[0], phis_all[0], bins=bins, normed=False, range=[[-180,180]]*2)
-heatmap2, xedges, yedges = np.histogram2d(psis_all[1], phis_all[1], bins=bins, normed=False, range=[[-180,180]]*2)
-#print heatmap
-
-heatmap1 = heatmap1/(heatmap1.sum())
-heatmap2 = heatmap2/(heatmap2.sum())
+with open(unpickle_files[0]) as unpickle_file:
+    heatmap1 = pickle.load(unpickle_file)
+    heatmap1 = heatmap1/(heatmap1.max())
+with open(unpickle_files[1]) as unpickle_file:
+    heatmap2 = pickle.load(unpickle_file)
+    heatmap2 = heatmap2/(heatmap2.max())
 
 diff_map = heatmap1 - heatmap2
-
 maximum = max( diff_map.max(), -diff_map.min() )
+
+print diff_map
 
 #################
 #  Plot in plt  #
